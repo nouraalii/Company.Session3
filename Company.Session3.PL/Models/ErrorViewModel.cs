d7f59c7f02fef57302a808b38a23d708ad1ec8b8
nouraalii <noraali18032003@gmail.com>
2025-03-07 20:21:57 +0200

    Add project files.

---
+++ Company.Session3.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Company.Session3.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
