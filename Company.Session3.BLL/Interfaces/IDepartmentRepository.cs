5bd5ee1233aeb9433e5a98af26d350d2e3baf702
nouraalii <noraali18032003@gmail.com>
2025-03-07 22:37:12 +0200

    Department Module BLL

---
+++ Company.Session3.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Session3.DAL.Models;
+
+namespace Company.Session3.BLL.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+
+        Department? Get(int id);
+
+        int Add(Department model);
+        int Update(Department model);
+        int Delete(Department model);
+    }
+}
