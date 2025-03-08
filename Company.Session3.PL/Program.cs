dcf95228d584dc1d52facaf6d4544bc96aca3d75
nouraalii <noraali18032003@gmail.com>
2025-03-08 02:31:44 +0200

    Create Department Controller And Allow DI For All Services

--- Company.Session3.PL/Program.cs
+++ Company.Session3.PL/Program.cs
@@ -1,3 +1,8 @@
+using Company.Session3.BLL.Interfaces;
+using Company.Session3.BLL.Repositiories;
+using Company.Session3.DAL.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 namespace Company.Session3.PL
 {
     public class Program
@@ -8,6 +13,11 @@ namespace Company.Session3.PL
 
             // Add services to the container.
             builder.Services.AddControllersWithViews(); //Register Built-in MVC Services
+            builder.Services.AddScoped<IDepartmentRepository,DepartmentRepository>(); //Allow DI for DepartmentRepository
+            builder.Services.AddDbContext<CompanyDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });  //Allow DI for CompanyDbContext
 
             var app = builder.Build();
 
