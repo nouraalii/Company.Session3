90cfc1f480d5835ae578e12079a213ffd33fbe5c
nouraalii <noraali18032003@gmail.com>
2025-03-10 02:21:47 +0200

    Implementation Employee.ServiceBLL

---
+++ Company.Session3.BLL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Session3.DAL.Models;
+
+namespace Company.Session3.BLL.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        IEnumerable<T> GetAll();
+
+        T? Get(int id);
+
+        int Add(T model);
+        int Update(T model);
+        int Delete(T model);
+    }
+}
