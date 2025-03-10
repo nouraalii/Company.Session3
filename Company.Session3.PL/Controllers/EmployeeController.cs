4a263c0a1b115f0b55030258338dc7d2fcb35e2a
nouraalii <noraali18032003@gmail.com>
2025-03-10 04:13:12 +0200

    Implementation Employee PL

---
+++ Company.Session3.PL/Controllers/EmployeeController.cs
@@ -0,0 +1,114 @@
+﻿using Company.Session3.BLL.Interfaces;
+using Company.Session3.DAL.Data.Contexts;
+using Company.Session3.DAL.Models;
+using Company.Session3.PL.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.Session3.PL.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var employees = _employeeRepository.GetAll();
+            return View(employees);
+        }
+
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateEmployeeDto model)
+        {
+            if (ModelState.IsValid) 
+            {
+                var employee = new Employee
+                {
+                    Name = model.Name,
+                    Address = model.Address,
+                    Age = model.Age,
+                    CreateAt = model.CreateAt,
+                    Email = model.Email,
+                    HiringDate = model.HiringDate,
+                    IsActive = model.IsActive,
+                    IsDeleted = model.IsDeleted,
+                    Phone = model.Phone,
+                    Salary= model.Salary,
+                };
+                var count = _employeeRepository.Add(employee);
+                if (count > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? id , string ViewName = "Details") 
+        {
+            if (id is null) return BadRequest();
+            var employees = _employeeRepository.Get(id.Value);
+            if(employees is null) return NotFound(new { StatusCode = 404, message = $"Employee With Id  : {id} is not found" });
+            return View(ViewName,employees);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id) 
+        {
+            return Details(id, "Edit");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int id, Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id != employee.Id) return BadRequest(); // 400 
+                var count = _employeeRepository.Update(employee);
+                if (count > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            return Details(id, "Delete");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete([FromRoute] int id, Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id != employee.Id) return BadRequest(); // 400 
+                var count = _employeeRepository.Delete(employee);
+                if (count > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Company.Session3.PL/Dtos/CreateEmployeeDto.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Company.Session3.PL.Dtos
+{
+    public class CreateEmployeeDto
+    {
+        [Required (ErrorMessage = "Name is Required !!")]
+        public string Name { get; set; }
+
+        [Range (22,60 , ErrorMessage ="Age must be between 22 and 60")]
+        public int? Age { get; set; }
+
+        [DataType(DataType.EmailAddress , ErrorMessage ="Email is not vaild!!")]
+        public string Email { get; set; }
+
+        [RegularExpression(@"[0-9]{1,3}-[a-zA-Z]{5,10}-[a-zA-Z]{4,10}-[a-zA-Z]{5,10}$",
+                                    ErrorMessage ="Address Must be like 123-Street-City-Country")]
+        public string Address { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Salary { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+
+        [DisplayName("Hiring Date")]
+        public DateTime HiringDate { get; set; }
+
+        [DisplayName("Date Of Creation")]
+
+        public DateTime CreateAt { get; set; }
+    }
+}
